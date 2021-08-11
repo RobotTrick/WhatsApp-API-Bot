c8d807ae86e9174204147b566911e475b8aab849
David Lev <davidlev050414@gmail.com>
2021-08-12 02:10:27 +0300

    String changes & commands handlers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from urllib import parse
-
+import json
 from pyrogram import Client, filters, types
 import configparser
 from re import search
@@ -18,6 +18,39 @@ def create_url(phone: str, text: str) -> str:
     if phone.startswith("05"):
         phone = phone.replace("0", "972", 1)
     return format_url.format(phone, parse.quote(text))
+
+
+def save_user(uid: int):
+    """ save user/chat id to DB """
+    save_uses()
+    ids_file = "users.json"
+    try:
+        with open(ids_file, "r") as oFile:
+            users: list = json.load(oFile)
+    except FileNotFoundError:
+        users = []
+
+    if uid not in users:
+        users.append(uid)
+
+    with open(ids_file, "w") as nFile:
+        json.dump(users, nFile, indent=4)
+
+
+def save_uses():
+    """ save count of uses to DB """
+    uses_file = "uses.txt"
+    try:
+        with open(uses_file) as usesO:
+            uses = int(usesO.read())
+    except FileNotFoundError:
+        with open(uses_file, "w") as usesO:
+            usesO.write("0")
+        with open(uses_file) as usesO:
+            uses = int(usesO.read())
+    uses += 1
+    with open(uses_file, "w") as usesO:
+        usesO.write(str(uses))
 
 
 # Ask replay on messages with phone number
@@ -42,25 +75,29 @@ def replay_url(_, message: types.Message):
 
     url = create_url(number, message.text)
     # send message with the url
-    message.reply(lang_msg(message, "replay_url").format(url),
+    message.reply(lang_msg(message, "replay_url").format(f"`{url}`"),
                   reply_markup=types.InlineKeyboardMarkup([[
                       types.InlineKeyboardButton(lang_msg(message, "url_btn"), url=url),
                       types.InlineKeyboardButton(lang_msg(message, "share_btn"),
                                                  switch_inline_query=f"{number} {message.text}")
                   ]]))
+    save_user(message.chat.id)
+
+
+valid_re = r"^(?P<number>[0-9+]{10,16}) (?P<text>.*)"
 
 
 # handle inline queries
-@app.on_inline_query(filters.regex(r"^(?P<number>[0-9+]{10,16}) (?P<text>.*)"))
+@app.on_inline_query(filters.regex(valid_re))
 def inline(_, query: types.InlineQuery):
     match = query.matches[0].groupdict()
     url = create_url(match['number'], match['text'])
 
     query.answer([types.InlineQueryResultArticle(
-        title=f"📞 {match['number']} • {match['text']} 📝",
+        title=f"{match['number']} • {match['text']}",
 
         input_message_content=types.InputTextMessageContent(
-            lang_msg(query, "replay_url").format(url)
+            lang_msg(query, "replay_url").format(f"`{url}`")
         ),
 
         reply_markup=types.InlineKeyboardMarkup([[
@@ -73,6 +110,13 @@ def inline(_, query: types.InlineQuery):
 
         thumb_url="https://telegra.ph/file/d89f5b180b532a85784a4.png"
     )])
+    save_user(query.from_user.id)
+
+
+@app.on_inline_query(~filters.regex(valid_re))
+def valid_re(_, query: types.InlineQuery):
+    query.answer([]
+                 , switch_pm_text=lang_msg(query, "input_sug"), switch_pm_parameter="help")
 
 
 # When user replay to replay msg instead of phone-num msg
@@ -83,16 +127,31 @@ def do_not_replay_to_ask(_, message: types.Message):
 
 
 # start command
-@app.on_message(filters.command("start") & filters.private)
+@app.on_message(filters.command(["start", "help", "translate"]) & filters.private)
 def start(_, message: types.Message):
-    txt = lang_msg(message, "start_msg")
-    message.reply(txt.format(message.from_user.mention), disable_web_page_preview=True,
-                  reply_markup=types.InlineKeyboardMarkup([[
-                      types.InlineKeyboardButton(lang_msg(message, "repo"),
-                                                 url="https://github.com/david-lev/WhatsApp-API-Bot"),
-                      types.InlineKeyboardButton(lang_msg(message, "inline_btn"),
-                                                 switch_inline_query="")
-                  ]]))
+    if message.command == ["start"]:
+        txt = lang_msg(message, "start_msg")
+        message.reply(txt.format(message.from_user.mention), disable_web_page_preview=True,
+                      reply_markup=types.InlineKeyboardMarkup([[
+                          types.InlineKeyboardButton(lang_msg(message, "repo"),
+                                                     url=lang_msg(message, "url_repo")),
+                          types.InlineKeyboardButton(lang_msg(message, "inline_btn"),
+                                                     switch_inline_query_current_chat="")
+                      ]]))
+    elif "help" in message.command:
+        message.reply(lang_msg(message, "help_msg"),
+                      reply_markup=types.InlineKeyboardMarkup([[
+                          types.InlineKeyboardButton(lang_msg(message, "inline_btn"),
+                                                     switch_inline_query_current_chat="")
+                      ]]))
+    elif "translate" in message.command:
+        message.reply(lang_msg(message, "translate"),
+                      reply_markup=types.InlineKeyboardMarkup([[
+                          types.InlineKeyboardButton(lang_msg(message, "repo"),
+                                                     url=f"{lang_msg(message, 'url_repo')}/blob/main/strings.py"),
+                          types.InlineKeyboardButton(lang_msg(message, "support"),
+                                                     url="https://t.me/RobotTrickSupport")
+                      ]]))
 
 
 app.run()
--- strings.py
+++ strings.py
@@ -1,8 +1,7 @@
 from typing import Union
 from pyrogram.types import Message, InlineQuery
 
-# Here you can add your language strings. just add on every dict new key with your language code
-# and insert the value. after you done, scroll down and add your language code to the 'lang_msg' func check.
+# Here you can add your language strings. just add on every dict new key with your language code and insert the value.
 # keep you're mind that languages are displayed accordingly to your client (app, software) lang.
 
 strings = {
@@ -19,8 +18,8 @@ strings = {
         "he": "❌ הזינו מספר טלפון תקין בפורמט בינלאומי"
     },
     "replay_url": {
-        "en": "🔗 Your link is: `{}`",
-        "he": "🔗 הקישור שלך הוא: `{}`"
+        "en": "Click here to get/copy your link: {}",
+        "he": "לחץ כאן כדי לקבל/להעתיק את הקישור: {}"
     },
     "url_btn": {
         "en": "🔗 Url",
@@ -39,31 +38,84 @@ strings = {
         "he": "🔁 נסו באינליין!"
     },
     "start_msg": {
-        "en": "Hi {} 👋\n"
+        "en": "Hi {} 👋\n\n"
               "you can send me a phone number and text and I will return you a link that by clicking on it "
               "will "
               "be transferred to WhatsApp chat with the same number and with the text ready to send!\n"
-              "You can also use me Inline! Type in the bot user followed by the phone number and text.",
-        "he": "היי {} 👋\n"
-              " תוכל לשלוח לי מספר טלפון וטקסט ואני אחזיר לכם קישור שבלחיצה עליו תועברו לצ'אט וואטסאפ עם "
+              "You can also use me Inline! Type in the bot user followed by the phone number and text.\n"
+              "\n- For help send /help"
+              "\n- We need your help translating the bot! For more information send /translate"
+              "\n\n\n",
+        "he": "היי {} 👋\n\n"
+              "תוכל לשלוח לי מספר טלפון וטקסט ואני אחזיר קישור שבלחיצה עליו תועבר לצ'אט וואטסאפ עם "
               "אותו "
-              "המספר ועם הטקסט מוכן לשליחה!\n"
-              "ניתן להשתמש בי גם באינליין! הקלידו את יוזר הבוט ולאחריו את מספר הטלפון והטקסט."
+              "המספר ועם הטקסט מוכן לשליחה.\n"
+              "ניתן להשתמש בי גם באינליין! הקלד את יוזר הבוט ולאחריו את מספר הטלפון והטקסט."
+              "\n\n- לעזרה שלחו /help"
+              "\n- אנו זקוקים לעזרתכם בתרגום הבוט! למידע נוסף שלחו /translate"
     },
     "repo": {
         "en": "🛠 GitHub",
         "he": "🛠 גיטהאב"
+    },
+    "url_repo": {
+        "en": "https://github.com/RobotTrick/WhatsApp-API-Bot"
+    },
+    "support": {
+        "en": "👤 Support",
+        "he": "👤 תמיכה"
+    },
+    "input_sug": {
+        "en": "Enter input like +97212345678 Hello from Telegram",
+        "he": "הזינו טלפון וטקסט: 97212345678+ טקסט"
+    },
+    "help_msg": {
+        "en": "This bot allows you to create direct links to WhatsApp chats with text ready to send."
+              "\n\nhere are two ways to use this bot:"
+              "\n1. --Messages and Replays:--"
+              "\n- Send the phone number."
+              "\n- Replay on the **number message** with text to send."
+              "\n- Get a link ready to click and to copy.\n"
+              "\n\n2. --Using Inline:--"
+              "\n- Type the bot username in any chat."
+              "\n- After that type a phone number and immediately after it a text to send."
+              "\n- Click on the result that will be displayed and you will get the link ready to click and to copy.",
+        "he": "רובוט זה מאפשר יצירת קישורים ישירים לצ'אט וואטסאפ עם טקסט מוכן לשליחה.\n"
+              "קיימות שתי צורות שימוש בבוט:\n\n"
+              "1. --הודעות ותגובות:--\n"
+              "- שלחו את מספר הטלפון.\n"
+              "- הגיבו על **המספר** עם טקסט לשליחה.\n"
+              "- קבלו קישור מוכן להקלקה ולהעתקה.\n\n"
+              "2. --שימוש באינליין:--\n"
+              "- הקלידו את יוזר הבוט בכל צ'אט שתרצו.\n"
+              "- לאחריו הקלידו מספר טלפון ומיד לאחריו טקסט לשליחה.\n"
+              "- הקליקו על התוצאה שתוצג ותקבלו את הקישור."
+    },
+    "translate": {
+        "en": "**🔡 We need your help translating the bot!**"
+              "\n\nIf you are interested in translating the bot into your language or to another language, go to the "
+              "strings file in GitHub, download it or edit it online and add the strings to the file according to the "
+              "existing format. "
+              "\n- Got tangled up, need help? Contact our support user.",
+        "he": "**🔡 אנו צריכים את עזרתכם בתרגום הבוט!**"
+              "\n\nאם הנכם מעוניינים לתרגם את הבוט לשפתכם או לשפה אחרת, עברו לקובץ ההודעות בגיטהאב, הורידו אותו או "
+              "ערכו "
+              "באונליין והוסיפו את המחרוזות לקובץ על פי הפורמט הקיים. \n\n- הסתבכתם, זקוקים לעזרה? פנו למשתמש התמיכה "
+              "שלנו. "
     }
 
 }
 
 
-def lang_msg(msg_obj: Union[Message, InlineQuery], msg_to_rpl: str) -> str:
+# Return message depend on the client language:
+def lang_msg(msg_obj: Union[Message, InlineQuery], msg_to_rpl: str) -> Union[str, bool]:
     msg = strings.get(msg_to_rpl)
-    if msg_obj.from_user.language_code == "he":
-        return msg["he"]
-    # Edit this if you want to add you're language to the bot:
-    # elif msg_obj.from_user.language_code == "YOUR_LANG_CODE":
-    #     return msg["YOUR_LANG_CODE"]
+
+    if not msg:
+        return False
+
+    lang_client = msg_obj.from_user.language_code
+    if msg.get(lang_client):
+        return msg[lang_client]
     else:
         return msg["en"]
